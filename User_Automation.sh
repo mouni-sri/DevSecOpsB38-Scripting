9dbc47d8cccf6170cac706238cde0f548d7cec9c
mouni-sri <mounikanagasri@gmail.com>
2023-12-20 22:56:36 +0530

    added random pwd in user creation

--- User_Automation.sh
+++ User_Automation.sh
@@ -6,14 +6,14 @@ if [ $# -gt 0 ]; then
         echo "the user $USERNAME already exists"
     else
         echo "lets create user $USERNAME"
+        sudo useradd -m ${USERNAME} --shell /bin/bash
+        sudo usermod -aG sudo ${USERNAME}
+        echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+        SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
+        PASSWORD="India@${RANDOM}${SPEC}"
+        echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
+        passwd -e ${USERNAME}
     fi
 else
     echo "PLease provide valid arguments."
-    sudo useradd -m ${USERNAME} --shell /bin/bash
-    sudo usermod -aG sudo ${USERNAME}
-    echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
-    SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
-    PASSWORD="India@${RANDOM}${SPEC}"
-    echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
-    passwd -e ${USERNAME}
 fi
