022c0d41695418c6361b3d121fc6b583eb4759ab
mouni-sri <mounikanagasri@gmail.com>
2023-12-20 22:17:32 +0530

    added if in user creation

--- User_Automation.sh
+++ User_Automation.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
-USERNAME=$1
-EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1) 
-if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
-    echo "the user $USERNAME already exists"
+if [ $# -gt 0 ]; then
+    USERNAME=$1
+    EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+    if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
+        echo "the user $USERNAME already exists"
+    else
+        echo "lets create user $USERNAME"
+    fi
 else
-    echo "lets create user $USERNAME"
+    echo "PLease provide valid arguments."
 fi
