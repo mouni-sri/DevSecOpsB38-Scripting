e9a61ab0c912c2cce4861e6d11187d6d792f2838
mouni-sri <mounikanagasri@gmail.com>
2023-12-23 22:34:24 +0530

    added multi user automation

---
+++ Multi_User_Automaion.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+if [ $# -gt 0 ]; then
+    for USERNAME in $@;
+    do
+    EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f 1)
+    if [ "${USERNAME}" = "${EXISTING_USER}" ]; then
+        echo "the user $USERNAME already exists"
+    else
+        echo "lets create user $USERNAME"
+        sudo useradd -m ${USERNAME} --shell /bin/bash
+        sudo usermod -aG sudo ${USERNAME}
+        echo '${USERNAME} ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+        SPEC=$(echo '!@#$%^&*()_' | fold -w1 | shuf | head -1)
+        PASSWORD="India@${RANDOM}${SPEC}"
+        echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
+        passwd -e ${USERNAME}
+        echo "the temporary credentials are ${USERNAME} and $PASSWORD"
+    fi
+    done
+else
+    echo "PLease provide valid arguments."
+fi
