17a69226fe97a44f1d430555f2fdd0a7267afd8a
mouni-sri <mounikanagasri@gmail.com>
2024-04-05 17:03:30 +0530

    added pallindrome script

---
+++ palindrome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+$STRING=$1
+$STR_LEN=$(expr ${#STRING} -1)
+$STR_REV=''
+echo $STR_LEN
+for ((i = $STR_LEN; i >= 0; i--)); do
+    X=${STRING[@]:$i:1}
+    $STR_REV=${STR_REV}$X
+done
+if [ ${STRING} = ${STR_REV}]; then
+    echo "$STRING is a pallindrome"
+else
+    echo "$STRING is not a pallindrome"
+fi
