ec11bdd9c60cbf786ebc60d0b7392e7c07d4b309
mouni-sri <mounikanagasri@gmail.com>
2024-04-05 17:08:35 +0530

    updated pallindrome script

--- palindrome.sh
+++ palindrome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-$STRING=$1
-$STR_LEN=$(expr ${#STRING} -1)
-$STR_REV=''
+STRING=$1
+STR_LEN=$(expr ${#STRING} -1)
+STR_REV=''
 echo $STR_LEN
 for ((i = $STR_LEN; i >= 0; i--)); do
     X=${STRING[@]:$i:1}
