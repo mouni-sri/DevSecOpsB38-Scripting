e78bae417a30cc01c08fe370c66666f01f8f933c
mouni-sri <mounikanagasri@gmail.com>
2024-04-11 00:22:49 +0530

    added regex

--- palindrome.sh
+++ palindrome.sh
@@ -7,7 +7,7 @@ for ((i = $STR_LEN; i >= 0; i--)); do
     X=${STRING[@]:$i:1}
     STR_REV=${STR_REV}$X
 done
-if [ ${STRING} = ${STR_REV}]; then
+if [ ${STRING} = ${STR_REV} ]; then
     echo "$STRING is a pallindrome"
 else
     echo "$STRING is not a pallindrome"
